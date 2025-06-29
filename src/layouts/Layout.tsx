85a43ab10873ac1e6075b9cf3ea187af6551acf4
Yisus550 <jesus.alfonso2402@gmail.com>
2025-06-29 12:28:52 -0700

    Refactor Layout component to use motion for animated transitions

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,18 +1,24 @@
+import { motion } from "motion/react";
 import { Outlet } from "react-router-dom";
+import { itemVariants } from "../main";
 
 export default function Layout() {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <header className="bg-slate-800">
         <div className="max-w-6xl py-10 mx-auto">
-          <h1 className="text-4xl font-extrabold text-white">
+          <motion.h1 variants={itemVariants} className="text-4xl font-extrabold text-white">
             Administradaor de Productos
-          </h1>
+          </motion.h1>
         </div>
       </header>
       <main className="max-w-6xl p-10 mx-auto mt-10 bg-white shadow">
         <Outlet />
       </main>
-    </>
+    </motion.div>
   );
 }

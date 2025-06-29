7769eaf32cd2fd31242553a1ff8493f8668097a4
Yisus550 <jesus.alfonso2402@gmail.com>
2025-06-29 13:07:05 -0700

    refactor: Enhance layout and styling in EditProduct component for improved user experience

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -54,23 +54,42 @@ export default function EditProduct() {
   const error = useActionData() as string;
 
   return (
-    <motion.div variants={itemVariants} initial="hidden" animate="visible">
-      <div className="flex justify-between">
+    <motion.div
+      variants={itemVariants}
+      initial="hidden"
+      animate="visible"
+      className="grid grid-cols-3"
+    >
+      <div className="flex justify-between col-span-3">
         <h2 className="text-4xl font-black text-slate-500">Editar Producto</h2>
         <Link
           to="/"
-          className="p-3 text-sm font-bold text-white bg-indigo-600 rounded shadow-sm hover:bg-indigo-500"
+          className="flex gap-4 px-6 py-3 text-sm font-bold text-indigo-600 transition-colors border border-indigo-600 rounded-lg shadow-sm hover:text-white hover:bg-indigo-600"
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+            />
+          </svg>
           Volver a productos
         </Link>
       </div>
 
       {error && <ErrorMessage>{error}</ErrorMessage>}
 
-      <Form className="mt-10" method="POST">
+      <Form className="col-span-2 p-6 mt-10 bg-white rounded-lg" method="POST">
         <ProductForm product={product} />
 
-        <label className="text-gray-800" htmlFor="availability">
+        <label className="font-bold text-gray-800" htmlFor="availability">
           Disponibilidad:
         </label>
         <select
